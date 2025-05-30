f0e904cc97918f544fb4601adafec063db1b6808
kurebayashi rento <kurebayashi.rat@gmail.com>
2025-05-30 14:34:39 +0900

    a

--- cs0530_2/Form1.cs
+++ cs0530_2/Form1.cs
@@ -2,9 +2,15 @@ namespace cs0530_2
 {
     public partial class Form1 : Form
     {
+        int vx=10, vy=10;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
         }
     }
 }
